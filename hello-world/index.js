6cfa6daa1b52396c53d06ed6c0c92bcd48f2c1ea
Prat <banqfirn@gmail.com>
2015-05-02 12:53:54 -0400

    Add alert

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,6 +1,10 @@
 var $ = require('dombo')
 var mkdirp = require('mkdirp').sync
 
+$('#hello-button').on('click', function () {
+  window.alert("hello, world!");
+})
+
 $('#foo-button').on('click', function () {
   mkdirp('./foo', function(err) {
      console.log(err);
