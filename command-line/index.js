899d198b150c236435a51b892a7476af649994e9
Prat <banqfirn@gmail.com>
2015-05-02 16:43:40 -0400

    Try out adbkit

--- command-line/index.js
+++ command-line/index.js
@@ -1,5 +1,8 @@
-var $ = require('dombo')
+var $ = require('dombo');
 var spawn = require('child_process').spawn;
+var Promise = require('bluebird');
+var adb = require('adbkit');
+var client = adb.createClient();
 
 $('#ls-button').on('click', function () {
   console.log('ls');
@@ -8,7 +11,8 @@ $('#ls-button').on('click', function () {
 
 $('#run-button').on('click', function () {
   console.log('run');
-  run_cmd( "adb", ["devices"], function(code) { console.log (code) });
+  //run_cmd( "adb", ["devices"], function(code) { console.log (code) });
+  listFiles();
 })
 
 function list() {
@@ -28,10 +32,36 @@ function list() {
 }
 
 function run_cmd(cmd, args, callBack ) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
+  var spawn = require('child_process').spawn;
+  var child = spawn(cmd, args);
+  var resp = "";
 
-    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-    child.stdout.on('end', function() { callBack (resp) });
+  child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+  child.stdout.on('end', function() { callBack (resp) });
+}
+
+//client.screencap(serial[, callback])
+
+function listFiles() {
+  client.listDevices()
+  .then(function(devices) {
+    return Promise.map(devices, function(device) {
+      return client.readdir(device.id, '/sdcard')
+      .then(function(files) {
+        // Synchronous, so we don't have to care about returning at the
+        // right time
+        files.forEach(function(file) {
+          if (file.isFile()) {
+            console.log(device.id + ' Found file ' + file.name)
+          }
+        })
+      })
+    })
+  })
+  .then(function() {
+    console.log('Done checking /sdcard files on connected devices')
+  })
+  .catch(function(err) {
+    console.error('Something went wrong:', err.stack)
+  })
 }
