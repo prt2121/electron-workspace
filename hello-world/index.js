30c5ced0b8b1f402fedf5a196d17bb303fa2a085
Prat <banqfirn@gmail.com>
2015-05-02 23:00:01 -0400

    Add more UI & RxJS

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,9 @@
 var $ = require('dombo')
 var mkdirp = require('mkdirp').sync
+var Rx = require('rx');
+var input = $('#input');
+var copy = document.querySelector('#copy-input');
+var content = document.querySelector('#content-container');
 
 $('#hello-button').on('click', function () {
   window.alert("hello, world!");
@@ -7,6 +11,24 @@ $('#hello-button').on('click', function () {
 
 $('#foo-button').on('click', function () {
   mkdirp('./foo', function(err) {
-     console.log(err);
+    console.log(err);
   });
 })
+
+var keyups = Rx.Observable.fromEvent(input, 'keyup')
+.map(function (e) {
+  return e.target.value;
+});
+
+keyups.subscribe( function (data) {
+  /* Do something with the data like binding */
+  log(data);
+}, function (error) {
+  /* handle any errors */
+  console.log(error);
+});
+
+function log(value){
+  copy.value = value;
+  content.innerHTML = value;
+}
