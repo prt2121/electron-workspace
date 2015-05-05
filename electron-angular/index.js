1c1d87b2df9fca7d88c79587f84d6636d927ea9b
Prat <banqfirn@gmail.com>
2015-05-04 23:16:12 -0400

    Add material

--- electron-angular/index.js
+++ electron-angular/index.js
@@ -1,6 +1,10 @@
 var angular = require('angular');
+var angularMaterial = require('angular-material');
+var angularAnimate = require('angular-animate');
+var angularAria = require('angular-aria');
 
-angular.module('myApp', [])
+angular.module('myApp', ['ngMaterial', 'ngAnimate', 'ngAria'])
+.config(function($mdThemingProvider) { $mdThemingProvider.theme('indigo').dark();})
 .controller('MyController', ['$scope', function ($scope) {
   $scope.greetMe = 'World';
 }]);
